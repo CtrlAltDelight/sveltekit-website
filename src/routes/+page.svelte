f0547f2c1e5c0d94078443412c936c21ac61cdba
CtrlAltDelight <lbchigges@gmail.com>
2023-04-01 14:45:42 -0400

    Made some formatting for the inner box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import links from '$lib/links.json';
+</script>
+
 <style>
   @import '../global.css';
 </style>
@@ -8,7 +12,27 @@
 	</header>
 	<div class="container">
 		<div class="box">
-			<p>Welcome to my website!</p>
+
+			<div class="header-container gradient-background">
+				<p>Welcome!</p>
+			</div>
+
+			<div class="inner-column" id="link-box">
+				<p id="text03">Sogist</p>
+				<p id="text04" style="text-align: center">programmer, gamer, teacher</p>
+				<hr>
+				<p id="text01"><b>likes:</b> keyboards, computers, etc.</p>
+			</div>
+
+			<div class="inner-column">
+				<p>Links</p>
+				<ul>
+					{#each links as link}
+						<li><a href={link.url}>{link.name}</a></li>
+					{/each}
+				</ul>
+			</div>
+
 		</div>
 	</div>
 	<footer>
