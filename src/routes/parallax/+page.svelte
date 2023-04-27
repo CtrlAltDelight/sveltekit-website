c854e7b016846dfb56c23ed5278a28050914c9a9
CtrlAltDelight <lbchigges@gmail.com>
2023-04-27 14:41:47 -0400

    Edited some text and added a return to homepage link

--- src/routes/parallax/+page.svelte
+++ src/routes/parallax/+page.svelte
@@ -7,7 +7,7 @@
 <div class="text-container">
 	<h2>SCROLLING</h2>
 	<p>
-		Every web developer asks the question: "How do you make a website look good?"<br><br>The answer lies not within the skill of the developer, or in the creativity of the person. Instead, the answer lies within their ability to search the internet for photos that look cool and then string them together haphazardly.
+		Every web developer asks the question: "How do you make a website look good?"<br><br>The answer lies not within the skill of the developer, or in their creativity.<br><br> Instead, the answer lies within their ability to search the internet for photos that look cool and then string them together haphazardly.
 	</p>
 </div>
 
@@ -15,7 +15,7 @@
 
 <div class="text-container">
 	<p>
-	See?<br><br>And if you feel adventurous you can even put the text...
+		See?<br><br>And if you feel adventurous you can even put the text...
 	</p>
 </div>
 
@@ -23,3 +23,10 @@
 <div class="parallax" id=image4>
 	<h2>ON IMAGES</h2>
 </div>
+
+<div class="text-container">
+	<p>
+		And now you may return to the <a style="padding:0px" href="/">homepage</a>
+	</p>
+</div>
+
